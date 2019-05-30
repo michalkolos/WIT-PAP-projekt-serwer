2ea252c1be3aaaf443f7d4d3f429723a4ebbe36e
Michal Kolosowski <cool.kolos@gmail.com>
2019-05-30 17:08:14 +0200

    WIP

--- src/connection.c
+++ src/connection.c
@@ -1,17 +1,68 @@
 #include <stdio.h>
 #include <errno.h>
+#include <pthread.h>
 
-#define BUFFER_LEN 512
+#define BUFFER_LEN 1024
 
 typedef struct Message Message;
+typedef struct Block Block;
 
 struct Message{
-    char buffer [BUFFER_LEN];
-    int length;
+    Block*  start;
+    Block*  end; 
+    int     blockLen;
+
+   	pthread_mutex_t mutex;
+
+    int totalLen;
     int position;
 };
 
+struct Block{
+    char    string [BUFFER_LEN];
+    Block*  nextBlock;
+};
 
+void messageInit(Message* message, int blockLen){
+    message->totalLen = 0;
+    message->position = 0;
+    message->blockLen = blockLen;
+
+    Block* newBlock = malloc(sizeof(Block));
+    if(newBlock == NULL){
+        // TODO: Adding connection to queue malloc error handling.
+    } 
+    message->start = newBlock;
+    message->end = newBlock;
+
+    pthread_mutex_init(&message->mutex, NULL);
+
+    return;
+}
+
+void messageAddNewBlock(Message* message, char* buffer, int size){
+    
+    
+
+    Block* newBlock = malloc(sizeof(Block));
+    if(newBlock == NULL){
+        // TODO: Adding connection to queue malloc error handling.
+    }
+
+    newBlock->nextBlock = bufer;
+
+    pthread_mutex_lock(message->mutex);
+
+    return;
+}
+
+void messageWrite(Message* message, char* buffer, int size){
+
+    int noOfFullBlocks = message->blockLen % size;
+    
+
+    for ( int i = 0; i < )
+}
 
 void readFromSocket(int socket, char* buffer, int size){
 
