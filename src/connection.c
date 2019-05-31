41247facf8d01e03e30f8c1f5d267283eda1641f
Michal Kolosowski <cool.kolos@gmail.com>
2019-06-01 01:23:03 +0200

    Deleted obsolete custom String structs.

--- src/connection.c
+++ src/connection.c
@@ -4,65 +4,8 @@
 
 #define BUFFER_LEN 1024
 
-typedef struct Message Message;
-typedef struct Block Block;
-
-struct Message{
-    Block*  start;
-    Block*  end; 
-    int     blockLen;
-
-   	pthread_mutex_t mutex;
-
-    int totalLen;
-    int position;
-};
-
-struct Block{
-    char    string [BUFFER_LEN];
-    Block*  nextBlock;
-};
-
-void messageInit(Message* message, int blockLen){
-    message->totalLen = 0;
-    message->position = 0;
-    message->blockLen = blockLen;
-
-    Block* newBlock = malloc(sizeof(Block));
-    if(newBlock == NULL){
-        // TODO: Adding connection to queue malloc error handling.
-    } 
-    message->start = newBlock;
-    message->end = newBlock;
-
-    pthread_mutex_init(&message->mutex, NULL);
-
-    return;
-}
-
-void messageAddNewBlock(Message* message, char* buffer, int size){
-    
-    
-
-    Block* newBlock = malloc(sizeof(Block));
-    if(newBlock == NULL){
-        // TODO: Adding connection to queue malloc error handling.
-    }
-
-    newBlock->nextBlock = bufer;
-
-    pthread_mutex_lock(message->mutex);
-
-    return;
-}
-
-void messageWrite(Message* message, char* buffer, int size){
-
-    int noOfFullBlocks = message->blockLen % size;
 
 
-    for ( int i = 0; i < )
-}
 
 void readFromSocket(int socket, char* buffer, int size){
 
@@ -103,9 +46,9 @@ char switchBoard(char* buffer, int size){
 
 
 
-"data":{[
-    {   "table1": ["col01":{"name", "type"},
-                "col02":{"name", "type"}]
-        "table2": ["col01":{"name", "type"},
-                "col02":{"name", "type"}]
-    ] }
+// "data":{[
+//     {   "table1": ["col01":{"name", "type"},
+//                 "col02":{"name", "type"}]
+//         "table2": ["col01":{"name", "type"},
+//                 "col02":{"name", "type"}]
+//     ] }
