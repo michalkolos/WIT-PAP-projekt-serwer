00c1ae853a75e49c5ab093011bb5b22661aad9b0
Michal Kolosowski <cool.kolos@gmail.com>
2019-06-02 01:48:51 +0200

    Placed all definitions from log.c into log.h file.

--- include/log.h
+++ include/log.h
@@ -1,7 +1,69 @@
 #ifndef LOG_H
 #define LOG_H
 
+
+
+// 8""8""8                               
+// 8  8  8 eeeee eeee eeeee  eeeee eeeee 
+// 8e 8  8 8   8 8  8 8   8  8  88 8   " 
+// 88 8  8 8eee8 8e   8eee8e 8   8 8eeee 
+// 88 8  8 88  8 88   88   8 8   8    88 
+// 88 8  8 88  8 88e8 88   8 8eee8 8ee88 
+                                      
+// ======================================
+
+
+#define LOG_MESSAGE_LEN 512
+
 #define log(...) logm(__VA_ARGS__, NULL)
+
+
+
+
+
+// 8""""8                                                       
+// 8      eeeee eeeee  e   e eeee eeeee e   e eeeee  eeee eeeee 
+// 8eeeee   8   8   8  8   8 8  8   8   8   8 8   8  8    8   " 
+//     88   8e  8eee8e 8e  8 8e     8e  8e  8 8eee8e 8eee 8eeee 
+// e   88   88  88   8 88  8 88     88  88  8 88   8 88      88 
+// 8eee88   88  88   8 88ee8 88e8   88  88ee8 88   8 88ee 8ee88 
+
+// ============================================================
+
+typedef struct LogQueue LogQueue;
+typedef struct LogMessage LogMessage;
+
+struct LogQueue{
+    LogMessage* tail;
+    LogMessage* head;
+
+    int size; 
+
+   	pthread_mutex_t mutex;             // Takes care of access to the queeue.
+    pthread_cond_t  conditionVar;
+    pthread_t       loggingThreadId;
+};
+
+
+struct LogMessage{
+    struct timespec timestamp;
+    int             level;
+    char            string[LOG_MESSAGE_LEN];
+    
+    LogMessage*     nextMessage;
+};
+
+
+
+// 8""""8                                                        
+// 8    8 eeeee  eeeee eeeee eeeee eeeee e    e eeeee eeee eeeee 
+// 8eeee8 8   8  8  88   8   8  88   8   8    8 8   8 8    8   " 
+// 88     8eee8e 8   8   8e  8   8   8e  8eeee8 8eee8 8eee 8eeee 
+// 88     88   8 8   8   88  8   8   88    88   88    88      88 
+// 88     88   8 8eee8   88  8eee8   88    88   88    88ee 8ee88 
+                                                              
+// =============================================================
+
 
 void logm(int level, const char *str, ...);
 
--- src/log.c
+++ src/log.c
@@ -3,118 +3,49 @@
 #include <pthread.h>
 #include <time.h>
 #include <string.h>
+#include <stdlib.h>
 
-// #define log(...) logm(INT, __VA_ARGS__, NULL)
-
-
-#define LOG_MESSAGE_LEN 512
-
-typedef struct LogQueue LogQueue;
-typedef struct LogMessage LogMessage;
-
-struct LogQueue{
-    LogMessage* tail;
-    LogMessage* head;
-
-    int size; 
-
-   	pthread_mutex_t mutex;             // Takes care of access to the queeue.
-    pthread_cond_t  conditionVar;
-};
+#include "log.h"
+ 
 
 
-struct LogMessage{
-    struct timespec timestamp;
-    int             level;
-    char            string[LOG_MESSAGE_LEN];
-    
-    LogMessage*     nextMessage;
-};
+
 
 // unsigned long time_in_micros = 1000000 * tv.tv_sec + tv.tv_usec;
+ 
 
-/**
- * @brief 
- * 
- * @param message 
- * @param ... 
- */
+
+
 
 void logm(int level, const char *str, ...)
 {
+
+    struct timespec timestamp;
+    timespec_get (&timestamp, TIME_UTC);
+
+    LogMessage* message = malloc(sizeof(LogMessage));
+    if(message == NULL){
+        // TODO: Adding connection to queue malloc error handling.
+    }
+
+    message->level = level;
+    message->timestamp = timestamp;
+    message->string[0] = '\0';
+
     va_list arg;
 
     va_start(arg, str);
 
-    // printf("%s", va_arg(arg, const char *));
-    // printf("%s", va_arg(arg, const char *));
-    while (str) {
-        
-        // printf("%s", va_arg(arg, const char *));
- 
-        while (str) {
-        printf("%s", str);
+    while (str) { 
+        strncat(message->string, str, LOG_MESSAGE_LEN);
         str = va_arg(arg, const char *);
-    }
- 
     }
 
     va_end(arg);
 
+    printf("%s", message->string);
+
     return;
 }
 
-// void log(char* message, ...);
 
-
-// void log(int level, int count, ...){
-
-//     struct timespec timestamp;
-//     timespec_get (&timestamp, TIME_UTC);
-
-//     // char *strncat(char *dest, const char *src, size_t n);
-
-//     LogMessage* message = malloc(sizeof(LogMessage));
-//     if(message == NULL){
-//         // TODO: Adding connection to queue malloc error handling.
-//     }
-
-//     message->string[0] = '\0';
-
-//     va_list valist; 
-//     va_start(valist, count);
-
-//     va_arg(valist, char[LOG_MESSAGE_LEN]); 
-  
-//     va_end(valist); 
-
-//     return;
-// }
-
-  
-// void log(char* string, ...) {
-
-//     struct timespec timestamp;
-//     timespec_get (&timestamp, TIME_UTC);
-    
-//     int i = 0; 
-//     while( string[i] != '\0' ){
-//         if( string[i] == '%'){ i++; }
-//     }
-
-//     va_list valist; 
-//     va_start(valist, i);
-
-//     i = 0;
-//     while( string[i] != '\0' ){
-
-        
-
-//         i++;
-//     }
-//     va_arg(valist, int); 
-  
-//     va_end(valist); 
-  
-//     return;
-// }
